74a19954df22bd57f191325f9e29cefd7b919af5
Prageeth <116965360+prageethmgunathilaka@users.noreply.github.com>
2025-02-10 22:51:22 +0700

    Add Hello World controller

---
+++ src/main/kotlin/com/example/demo/controller/HelloController.kt
@@ -0,0 +1,15 @@
+package com.example.demo.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): Map<String, String> {
+        return mapOf("message" to "Hello, World!")
+    }
+}
